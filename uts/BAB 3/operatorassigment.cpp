8ba9d5a705d5bc0ff365597e73eb16a33f9c24ea
nissanur11 <57209507+nissanur11@users.noreply.github.com>
2019-10-31 13:55:31 +0700

    Add files via upload

---
+++ uts/BAB 3/operatorassigment.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    a = 10;
+    b = 4;
+
+    cout << "a = ";
+    cout << a;
+    cout << endl;
+    cout << "b = ";
+    cout << b;
+    cout << endl;
+    return 0;
+}
